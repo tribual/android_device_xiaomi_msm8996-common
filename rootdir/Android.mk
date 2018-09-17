2670059b1f12e840ac11b676c4483f68939d46c0
Michael Bestas <mkbestas@lineageos.org>
2018-09-17 22:43:53 +0300

    corrections #2

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -76,6 +76,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.panel_info.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.panel_info.sh
+LOCAL_SRC_FILES    := bin/init.panel_info.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
